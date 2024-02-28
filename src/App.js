e79950ec19480e5e84bfab6bfdc0f73789abf6ba
Fugari <Panni_Irena@comcast.net>
2024-02-27 21:51:02 -0500

    second commit using Vanila JS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
-function App() {
-  // line below - imperative programming
-  const curr_tim= new Date().getFullYear();
-  return (
-    //the whole block below - declarative programming.
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* Code for current year */}
-        <span className="year"> { curr_tim } </span>
-      </header>
-    </div>
-  );
+export const App = () => {
+  const curr_year = new Date().getFullYear();
+  return createElement(
+    'div', 
+    {className: 'App'},
+    createElement('header', {className: 'App-header'},
+        createElement('img', {src: {logo}, className:'App-logo', alt:'logo'}),
+        createElement('p', null, 'Edit src/App.js and save to reload.'),
+        createElement('a', {className:'App-link', href:'https://reactjs.org', target:'_blank', rel:'noopener noreferrer'}, 'Learn React' ),
+        createElement('span', {className:'year'}, curr_year)
+      )
+    )
 }
 
-export default App;
